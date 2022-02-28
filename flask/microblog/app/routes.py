8412442dd5fbb3313c05bb3656424eea3ef37ce5
mlaks23 <mlaks@schools.nyc.gov>
2022-02-28 12:42:59 -0500

    updated files

--- flask/microblog/app/routes.py
+++ flask/microblog/app/routes.py
@@ -1,16 +1,18 @@
+from flask import render_template
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'username': 'Marisa'}
-    return '''
-<html>
-    <head>
-        <title>Home Page - Microblog</title>
-    </head>
-    <body>
-        <h1>Hello, ''' + user['username'] + '''!</h1>
-    </body>
-<html>'''
-    
+    return render_template('index.html', title='Home', user= user)
+
+@app.route("/image_css")
+def image_css():
+  return render_template("image_css.html")
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
