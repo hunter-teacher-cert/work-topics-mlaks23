308a4d5a893c68246c001f50f74f5c2bbc050341
mlaks23 <mlaks@schools.nyc.gov>
2022-03-14 17:13:47 -0400

    updated file with link to google collab

--- databases/databases_sql.py
+++ databases/databases_sql.py
@@ -1,3 +1,7 @@
+
+#This SQL JOIN query will retrieve the records of all of the unexcused period attendance absences in the school for the week of 1-24-22 sorted by student last name ascending.
+
+
 SELECT First, Last, StudentID, Grade, ScanTime, Status, CourseSection, Attendance, Period, Teacher
 FROM (
   SELECT scan.First, scan.Last, scan.StudentID, scan.Grade, ScanTime, Status, CourseSection, Attendance, Period, Teacher
@@ -7,3 +11,8 @@ FROM (
   WHERE Attendance="A"
   ORDER BY scan.LAST)
 AS allCuts
+
+
+
+# Link to google collab:
+# https://colab.research.google.com/drive/1ZPpocGnjOFJyIL39Rwr29cqaogOLdNnr#scrollTo=6XydmJWGYia4
