32438116298f176f2da26930a90c545006961814
mlaks23 <mlaks@schools.nyc.gov>
2022-03-14 16:13:42 -0400

    updated file

--- databases/databases_sql.py
+++ databases/databases_sql.py
@@ -1,10 +1,9 @@
-SELECT First, Last, studentID, Grade, Scantime, (Period=1) AS pd1, Date, CourseSection
-FROM
-(
-SELECT t1.First, t1.Last, t1.studentID, t1.Grade, t1.ScanTime, t1.Status, Date, CourseSection, Attendance, Teacher, Period
-FROM scan AS t1
-INNER JOIN periodAtt AS t2
-ON t1.studentID=t2.studentID AND SUBSTR(t1.scanTime, 1, 9)=t2.date
-WHERE Attendance="A"
-ORDER BY t1.Last ASC)
+SELECT First, Last, StudentID, Grade, ScanTime, Status, CourseSection, Attendance, Period, Teacher
+FROM (
+  SELECT scan.First, scan.Last, scan.StudentID, scan.Grade, ScanTime, Status, CourseSection, Attendance, Period, Teacher
+  FROM scan
+  LEFT JOIN periodAtt AS period
+  ON scan.StudentID=period.StudentID AND SUBSTR(scan.ScanTime, 1, 9)=period.Date
+  WHERE Attendance="A"
+  ORDER BY scan.LAST)
 AS allCuts
